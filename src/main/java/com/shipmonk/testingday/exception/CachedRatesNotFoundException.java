9445299774a4b093368430ca75bfb50f3dfb7f09
David Ulicny <ulicny.david@gmail.com>
2026-01-23 13:27:50 +0100

    exception

---
+++ src/main/java/com/shipmonk/testingday/exception/CachedRatesNotFoundException.java
@@ -0,0 +1,8 @@
+package com.shipmonk.testingday.exception;
+
+public class CachedRatesNotFoundException extends Exception {
+
+    public CachedRatesNotFoundException(String message) {
+        super(message);
+    }
+}
